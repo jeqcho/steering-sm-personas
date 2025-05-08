0e14ed08ce001552a010fe6c835897e6bf0b5606
Jay Chooi <ubuntu@ip-172-31-64-23.ec2.internal>
2025-05-08 16:27:08 +0000

    moved pii removal to another repo

--- pii/hashing/job_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=def-rrabba
-#SBATCH --time=0:30:00
-#SBATCH --mem-per-cpu=32G
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-
-python hash_did.py --folder_name "/home/s4yor1/scratch/pii_removed/processed_2_clusters"
-python hash_did.py --folder_name "/home/s4yor1/scratch/pii_removed/processed_100_clusters"
